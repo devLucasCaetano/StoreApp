e518251fd45b680ad09d5a7ac1abe1a33012508e
LucasCaetano <lucas_caetano123@hotmail.com>
2023-08-29 03:32:10 -0300

    feat: estrutura basica de carrinho

--- src/screens/Cart/CartScreen.tsx
+++ src/screens/Cart/CartScreen.tsx
@@ -1,11 +1,75 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import ProductsComponent from '../../components/Products/ProductsComponent';
+import {
+  AmountContent,
+  Container,
+  ProductsContent,
+  TotalAmount,
+} from './CartStyles';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {FlatList} from 'react-native';
+
+const productList = [
+  {
+    id: 1,
+    name: 'Camiseta',
+    price: '100',
+    description: 'Product 1 description',
+  },
+  {
+    id: 2,
+    name: 'Tênis',
+    price: '200',
+    description: 'Product 2 description',
+  },
+  {
+    id: 3,
+    name: 'Fone',
+    price: '300',
+    description: 'Product 3 description',
+  },
+];
+
+function sumTotalAmount(totalAmount: number, product: Array<any>) {
+  product.map(item => {
+    totalAmount += Number(item.price);
+  });
+  return totalAmount.toFixed(2);
+}
+
+let totalAmount = 0;
 
 function CartScreen(): JSX.Element {
+  const renderProductItem = ({
+    item,
+  }: {
+    item: {name: string; price: string; description: string};
+  }) => (
+    <ProductsComponent
+      name={item.name}
+      price={item.price}
+      description={item.description}
+    />
+  );
+
   return (
-    <View>
-      <Text>Cart Screen</Text>
-    </View>
+    <Container>
+      <ProductsContent>
+        <GestureHandlerRootView>
+          <FlatList
+            data={productList}
+            renderItem={renderProductItem}
+            keyExtractor={item => item.id.toString()}
+          />
+        </GestureHandlerRootView>
+      </ProductsContent>
+      <AmountContent>
+        <TotalAmount>
+          Total: R${sumTotalAmount(totalAmount, productList) || '0,00'}
+        </TotalAmount>
+        {/* todo btn  */}
+      </AmountContent>
+    </Container>
   );
 }
 
---
+++ src/screens/Cart/CartStyles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({theme}) => theme.COLORS.WHITE};
+  justify-content: space-between;
+`;
+
+export const ProductsContent = styled.View`
+  width: 100%;
+  max-height: 80%;
+  height: auto;
+  padding: 0 24px;
+`;
+
+export const AmountContent = styled.View`
+  width: 100%;
+  height: 20%;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  padding: 0 16px;
+  border-top-style: solid;
+  border-top-width: 1px;
+  border-top-color: ${({theme}) => theme.COLORS.GRAY_100};
+`;
+
+export const TotalAmount = styled.Text`
+  font-size: ${({theme}) => theme.FONT_SIZE.XL}px;
+  color: ${({theme}) => theme.COLORS.BLUE_500};
+  font-weight: bold;
+`;
